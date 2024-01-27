2c49f78ce99b215ca8a49cdfa2db0cfe3b700f63
MAHEER SIDDIQUI <137050164+Maheersiddiqui@users.noreply.github.com>
2024-01-27 10:37:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let passsword="123";
+let password_from_user="098";
+
+//  console.log(`your pay is ${pay}`);
+//  console.log("your pay is ${pay}");
+
+
+ if(passsword=="123")
+ {
+    console.log("correct password");
+ }
+ else if(passsword=="098"){
+console.log("wrong password")
+ }
+ 
+ else{
+    console.log("ïnvalid input");
+ }
+
+
+
+//  alert("welcome to my page");
+prompt("enter your name");
---
+++ app2.js
@@ -0,0 +1,10 @@
+let name="MAHEER siddiqui";
+let last_name=" siddiqui";
+let fullname=name+last_name;
+console.log(name+last_name);
+console.log("maheer "+"siddiqui")
+console.log(name.toUpperCase());
+let name_uppercase=name.toUpperCase();
+console.log(name_uppercase);
+console.log(name.toLowerCase());
+console.log(name.indexOf('E'));
